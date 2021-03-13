8efe66500120a17323ec72bbfca546b91d84bf58
Mehrdad <Mehrdad@Mehrdads-iMac.lan>
2021-03-13 06:32:40 +0330

    feat: Add text to document directory of the sandbox in SandboxPlayground

--- SandboxPlayground-Empty/SandboxPlayground/AppDelegate.swift
+++ SandboxPlayground-Empty/SandboxPlayground/AppDelegate.swift
@@ -16,11 +16,37 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: Properties
     
     var window: UIWindow?
+    
+    func sandboxPlayground() {
+        let fileManager = FileManager.default
+        let urls = fileManager.urls(for: .documentDirectory, in: .userDomainMask)
+        let url = urls.last?.appendingPathComponent("file.txt")
+        
+        do {
+            try "Hi There!".write(to: url!, atomically: true, encoding: .utf8)
+        } catch {
+            print("Error while writing")
+        }
+        
+        do {
+            let content = try String(contentsOf: url!, encoding: .utf8)
+            
+            if content == "Hi There!" {
+                print("yay")
+            } else {
+                print("oops")
+            }
+        } catch {
+            print("Something went wrong")
+        }
+    }
 
     // MARK: UIApplicationDelegate
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        sandboxPlayground()
         return true
     }
 }
