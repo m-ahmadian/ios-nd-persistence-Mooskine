ad4df38bde00ef49ed131d8b433ac767d0766b89
Mehrdad <Mehrdad@Mehrdads-iMac.lan>
2021-03-16 18:15:20 +0330

    feat: Inject the DataController dependency into NotebooksListViewController

--- Mooskine/Mooskine-starter/Mooskine/AppDelegate.swift
+++ Mooskine/Mooskine-starter/Mooskine/AppDelegate.swift
@@ -19,6 +19,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         dataController.load()
         
+        let navigationController = window?.rootViewController as! UINavigationController
+        let notebooksListViewController = navigationController .topViewController as! NotebooksListViewController
+        notebooksListViewController.dataController = dataController
+        
         return true
     }
 
