cce2e28c9b5924a0479bbf5563a9aee926706292
Mehrdad <mehrdad.ahmadian@gmail.com>
2021-03-29 06:43:25 +0430

    feat: Add a toolbar with buttons to prepare keyboard to edit text

--- Mooskine/Mooskine-starter/Mooskine/View Controllers/NoteDetailsViewController.swift
+++ Mooskine/Mooskine-starter/Mooskine/View Controllers/NoteDetailsViewController.swift
@@ -19,6 +19,9 @@ class NoteDetailsViewController: UIViewController {
 
     /// A closure that is run when the user asks to delete the current note
     var onDelete: (() -> Void)?
+    
+    /// The accessory view used when displaying the keyboard
+    var keyboardToolbar: UIToolbar?
 
     /// A date formatter for the view controller's title text
     let dateFormatter: DateFormatter = {
@@ -34,6 +37,10 @@ class NoteDetailsViewController: UIViewController {
             navigationItem.title = dateFormatter.string(from: creationDate)
         }
         textView.attributedText = note.attributedText
+        
+        // keyboard toolbar configuration
+        configureToolbarItems()
+        configureTextViewInputAccessoryView()
     }
 
     @IBAction func deleteNote(sender: Any) {
@@ -64,5 +71,56 @@ extension NoteDetailsViewController: UITextViewDelegate {
     func textViewDidEndEditing(_ textView: UITextView) {
         note.attributedText = textView.attributedText
         try? dataController.viewContext.save()
+    }
+}
+
+
+// MARK: - Toolbar
+
+extension NoteDetailsViewController {
+    /// Returns an array of toolbar items. Used to configure the view controller's
+    /// `toolbarItems' property, and to configure an accessory view for the
+    /// text view's keyboard that also displays these items.
+    func makeToolbarItems() -> [UIBarButtonItem] {
+        let trash = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(deleteTapped(sender:)))
+        let space = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        
+        return [trash, space]
+    }
+    
+    /// Configure the current toolbar
+    func configureToolbarItems() {
+        toolbarItems = makeToolbarItems()
+        navigationController?.setToolbarHidden(false, animated: false)
+    }
+    
+    /// Configure the text view's input accessory view -- this is the view that
+    /// appears above the keyboard. We'll return a toolbar populated with our
+    /// view controller's toolbar items, so that the toolbar functionality isn't
+    /// hidden when the keyboard appears
+    func configureTextViewInputAccessoryView() {
+        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height))
+        toolbar.items = makeToolbarItems()
+        textView.inputAccessoryView = toolbar
+    }
+    
+    @IBAction func deleteTapped(sender: Any) {
+        showDeleteAlert()
+    }
+    
+    
+    // MARK: Helper methods for actions
+    private func showDeleteAlert() {
+        let alert = UIAlertController(title: "Delete Note?", message: "Are you sure you want to delete the current note?", preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        let deleteAction = UIAlertAction(title: "Delete", style: .destructive) { [weak self] _ in
+            guard let strongSelf = self else { return }
+            strongSelf.onDelete?()
+        }
+        
+        alert.addAction(cancelAction)
+        alert.addAction(deleteAction)
+        present(alert, animated: true, completion: nil)
     }
 }
